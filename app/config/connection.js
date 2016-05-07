069ee3264e6a608ac809f15ee799321dd10b9a4d
John Reda <johnreda4@gmail.com>
2016-05-07 11:03:02 -0400

    first commit

---
+++ app/config/connection.js
@@ -0,0 +1,23 @@
+// *********************************************************************************
+// CONNECTION.JS - THIS FILE INITIATES THE CONNECTION TO MYSQL
+// *********************************************************************************
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    port: 3306,
+    host: 'l9dwvv6j64hlhpul.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+    user: 'ybw63hop83u3xsfp',
+    password: "lghi43b4bno1zhdv",
+    database: "it9n91i0arqs9kic"
+});
+
+
+connection.connect(function(err) {
+    if (err) {
+        console.error('error connecting: ' + err.stack);
+        return;
+    }
+    console.log('connected as id ' + connection.threadId);
+});
+
+module.exports = connection;
